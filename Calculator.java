7436ca2c8fbbb27e3881be584221a6f2e0096e9c
FarasatulMunawarah <farasatulmunawarah@gmail.com>
2021-04-20 12:11:16 +0700

    Calculator

---
+++ Calculator.java
@@ -0,0 +1,29 @@
+
+/**
+ * class Calculator
+ *
+ * @author FarasatulMunawarah
+ * @version 0.2
+ */
+public class Calculator{
+    String name;
+    
+    int a = 4;
+    int b = 5;
+    
+    int penjumlahan = a + b;
+    int perkalian = a * b;
+    int pengurangan = a - b;
+    int pembagian = a / b;
+    
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     
+    public int add(){
+        return x + y;
+    }*/
+}
